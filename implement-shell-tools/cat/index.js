f8ea2711cad42dc8906e4a7ac6922d4bc6346efc
katarzynakaz <l453r3@gmail.com>
2026-02-22 12:39:04 +0000

    functions separated and fixed

--- implement-shell-tools/cat/index.js
+++ implement-shell-tools/cat/index.js
@@ -1,87 +1,34 @@
-import process from "node:process";
 import { promises as fs } from "node:fs";
 
-
-//one for 1 and 3
-async function printOneOrMore(listOfFiles) {
-    for (const file of listOfFiles) {
-        const content = await fs.readFile(file, "utf-8");
-        console.log(content);
-    }
-}
-    
-
-// // 2 * `cat -n sample-files/1.txt`
-// async function caseTwo(oneFile) {
-//     const content = await fs.readFile(oneFile, "utf-8");
-//     const separatedToLines = content.split('\n')
-//     separatedToLines.forEach((line, index) => {
-//         console.log(`${index + 1} ${line}`)
-//     })
-// }
-
-// // 4 * `cat -n sample-files/*.txt`
-// async function caseFour(listOfFiles) {
-//     for (const file of listOfFiles) {
-//         const content = await fs.readFile(one, "utf-8");
-
-//         const separatedToLines = content.split('\n')
-//         separatedToLines.forEach((line, index) => {
-//         console.log(`${index + 1} ${line}`)
-//     })
-// }
-    
-// }
-
-//one instead
-//  2 * `cat -n sample-files/1.txt`
-// 4 * `cat -n sample-files/*.txt`
-
-async function caseTwoAndFour(listOfFiles) {
-    for (const file of listOfFiles) {
-        const content = await fs.readFile(file, "utf-8");
-
-        const separatedToLines = content.split('\n')
-        separatedToLines.forEach((line, index) => {
-            console.log(`${index + 1} ${line}`)
-    })
-}
-    
+async function cleanInput(listOfFiles) {
+	let cleanLinesArr = [];
+	for (const file of listOfFiles) {
+		const grabbedText = await fs.readFile(file, "utf-8");
+		const splitLines = grabbedText.split("\n");
+		cleanLinesArr.push(...splitLines);
+	}
+	return cleanLinesArr;
 }
 
-// `cat -b sample-files/3.txt`
-async function caseFive(listOfFiles) {
-    for (const file of listOfFiles) {
-        const content = await fs.readFile(file, "utf-8");
-        const separatedToLines = content.split('\n');
+function takeSpecifiedAction(cleanLinesArr, flag) {
+	let countingOnlyFullLines = 1;
 
-        let countingOnlyFullLines = 1
-        separatedToLines.forEach((line, index) => {
-            if (line !== '') {
-                console.log(`${countingOnlyFullLines} ${line}`)
-                countingOnlyFullLines++
-            } else {
-                console.log('')
-            }
-            
-        })
-    }
-
-}
-
-
-const argv = process.argv.slice(2);
-
-switch (argv[0]) {
-    case '-n':
-        await caseTwoAndFour(argv.slice(1));
-        break;
-
-    case '-b':
-        await caseFive(argv.slice(1));
-        break;
-
-    default:
-        await printOneOrMore(argv);
-        break;
+	for (const file of cleanLinesArr) {
+		// Task: We recommend you start off supporting no flags, then add support for `-n`, then add support for `-b`.
+		if (!flag) {
+			console.log(file);
+		} else if (flag === "-n") {
+			console.log(`${countingOnlyFullLines} ${file}`);
+			countingOnlyFullLines += 1;
+		} else if (flag === "-b") {
+			if (file === "") {
+				console.log("");
+			} else {
+				console.log(`${countingOnlyFullLines} ${file}`);
+				countingOnlyFullLines += 1;
+			}
+		} else {
+			console.log("incorrect flag");
+		}
+	}
 }
