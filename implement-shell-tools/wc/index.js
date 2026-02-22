4fb13cdcfa3877bc6aab627b1e97c832d129caf8
katarzynakaz <l453r3@gmail.com>
2026-02-22 13:31:19 +0000

    wc fixed

--- implement-shell-tools/wc/index.js
+++ implement-shell-tools/wc/index.js
@@ -9,69 +9,66 @@ import { promises as fs } from "node:fs";
 //   .length;
 // console.log(countOfWordsContainingEs);
 
-function countHelper(inputFiles) {
-    return {
-        lines: inputFiles.split('\n').length - 1,
-        words: inputFiles.split(' ').filter(w => w !== "").length,
-        bytes: inputFiles.length,
-    };
+function calculateCounts(inputFiles) {
+	return {
+		lines: inputFiles.split("\n").length - 1,
+		words: inputFiles.split(/\s+/).filter((w) => w !== "").length,
+		bytes: inputFiles.length,
+	};
 }
-
 
 // * `wc -l sample-files/3.txt`
 // * `wc -l sample-files/*`
 async function countLines(listOfFiles) {
-    for (const file of listOfFiles) {
-        const content = await fs.readFile(file, "utf-8");
+	for (const file of listOfFiles) {
+		const content = await fs.readFile(file, "utf-8");
 
-        // const linesNumbered = content.split('\n').length-1
-        const counts = countHelper(content);
-        console.log(`${counts.lines} ${file}`)
-    }
+		// const linesNumbered = content.split('\n').length-1
+		const counts = calculateCounts(content);
+		console.log(`${counts.lines} ${file}`);
+	}
 }
 
 // * `wc -w sample-files/3.txt`
 async function countWords(listOfFiles) {
-    for (const file of listOfFiles) {
-        const content = await fs.readFile(file, "utf-8");
-        
-        // const wordsCounted = content.split(" ").filter(word => word !== "").length;
-        // console.log(`${wordsCounted} ${file}`);
-        const counts = countHelper(content);
-        console.log(`${counts.words} ${file}`);
-}
-    
+	for (const file of listOfFiles) {
+		const content = await fs.readFile(file, "utf-8");
+
+		// const wordsCounted = content.split(" ").filter(word => word !== "").length;
+		// console.log(`${wordsCounted} ${file}`);
+		const counts = calculateCounts(content);
+		console.log(`${counts.words} ${file}`);
+	}
 }
 
 // * `wc -c sample-files/3.txt`
 async function countBytes(listOfFiles) {
-    for (const file of listOfFiles) {
-        const content = await fs.readFile(file, "utf-8");
-        // const bytesCounted = content.length;
-        const counts = countHelper(content);
-        console.log(`${counts.bytes} ${file}`);
-    }
+	for (const file of listOfFiles) {
+		const content = await fs.readFile(file, "utf-8");
+		// const bytesCounted = content.length;
+		const counts = calculateCounts(content);
+		console.log(`${counts.bytes} ${file}`);
+	}
 }
 
 // * `wc sample-files/*`
 async function countAll(listOfFiles) {
-    for (const file of listOfFiles) {
-        const content = await fs.readFile(file, "utf-8");
-        const counts = countHelper(content)
-
-        console.log(`${counts.lines} ${counts.words} ${counts.bytes} ${file}`);
-    }
+	for (const file of listOfFiles) {
+		const content = await fs.readFile(file, "utf-8");
+		const counts = calculateCounts(content);
+		console.log(`${counts.lines} ${counts.words} ${counts.bytes} ${file}`);
+	}
 }
 
 const argv = process.argv.slice(2);
-const files = argv.filter(arg => !arg.startsWith('-'));
+const files = argv.filter((arg) => !arg.startsWith("-"));
 
-if (argv.includes('-l')) {
-    await countLines(files);
-} else if (argv.includes('-w')) {
-    await countWords(files);
-} else if (argv.includes('-c')) {
-    await countBytes(files);
+if (argv.includes("-l")) {
+	await countLines(files);
+} else if (argv.includes("-w")) {
+	await countWords(files);
+} else if (argv.includes("-c")) {
+	await countBytes(files);
 } else {
-    await countAll(files);
+	await countAll(files);
 }
