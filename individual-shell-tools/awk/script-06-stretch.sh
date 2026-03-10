0993e2fb104e30673900d5473cb1e834b4ed98b0
HilolaRustamova <mohlaroy86@gmail.com>
2026-03-10 15:15:55 +0000

    awk

--- individual-shell-tools/awk/script-06-stretch.sh
+++ individual-shell-tools/awk/script-06-stretch.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 # NOTE: This is a stretch exercise - it is optional.
-
+awk '{sum += $3} END {print sum}' scores-table.txt
 # TODO: Write a command to output the total of adding together all players' first scores.
 # Your output should be exactly the number 54.
