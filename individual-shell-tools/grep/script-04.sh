5ea068a756ac54d7982880f4852d441c1088d8c0
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2026-03-11 16:54:38 +0000

    complete individual shell tools exercises

--- individual-shell-tools/grep/script-04.sh
+++ individual-shell-tools/grep/script-04.sh
@@ -2,5 +2,8 @@
 
 set -euo pipefail
 
+
+grep -v "Hello" dialogue.txt
+
 # TODO: Write a command to output every line in dialogue.txt that does not contain the word "Hello" (regardless of case).
 # The output should contain 10 lines.
