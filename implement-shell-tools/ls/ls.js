d4a4f43923616f80d4a369ebe48c9dd2a134d3fb
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2026-03-13 05:52:35 +0000

    feat(ls): add initial ls command to list current working directory contents

---
+++ implement-shell-tools/ls/ls.js
@@ -0,0 +1,27 @@
+import { program } from "commander";
+import * as fs from "node:fs/promises";
+
+program
+  .name("ls")
+  .description("List directory contents")
+  .option("-a", "Include directory entries whose names begin with a dot ('.').")
+  .option("-1", "Force output to be one entry per line.");
+
+program.parse();
+
+try {
+  const options = program.opts();
+
+  const currentDir = process.cwd();
+  const files = await fs.readdir(currentDir);
+
+  if (options["1"]) {
+    for (const file of files) {
+      console.log(file);
+    }
+  } else {
+    console.log(...files);
+  }
+} catch (err) {
+  console.error(err.message);
+}
