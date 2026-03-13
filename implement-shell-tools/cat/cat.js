d373c0a57f2fb5d30c8cf50dab368d0b7c8e683b
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2026-03-13 00:28:09 +0000

    feat(cat): enhance cat command with optional flags

--- implement-shell-tools/cat/cat.js
+++ implement-shell-tools/cat/cat.js
@@ -4,14 +4,27 @@ import * as fs from "node:fs/promises";
 program
   .name("cat")
   .description("Reads file and writes it to the standard output")
-  .argument("<path>", "The file path to process");
+  .argument("<path>", "The file path to process")
+  .option("-n", "Number the output lines, starting at 1.")
+  .option("-b", "Number only non-blank output lines, starting at 1.");
 
 program.parse();
 
 try {
   const [filePath] = program.args;
-  const contents = await fs.readFile(filePath, { encoding: "utf8" });
-  console.log(contents);
+  const options = program.opts();
+
+  const file = await fs.open(filePath);
+
+  let lineNum = 1;
+  for await (const line of file.readLines()) {
+    const isBlank = line.trim() === "";
+    const shouldNumber = options.n || (options.b && !isBlank);
+
+    console.log(shouldNumber ? `${lineNum}  ${line}` : line);
+
+    if (shouldNumber) lineNum++;
+  }
 } catch (err) {
   console.error(err.message);
 }
