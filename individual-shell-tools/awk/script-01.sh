726a0d41a15c9ccbe16421fb1db39d239ca1483b
TarasMykytiuk <tarasmykytiukw@gmail.com>
2026-03-16 00:06:11 +0200

    individual shell tools

--- individual-shell-tools/awk/script-01.sh
+++ individual-shell-tools/awk/script-01.sh
@@ -2,5 +2,7 @@
 
 set -euo pipefail
 
+
 # TODO: Write a command to output just the names of each player in `scores-table.txt`.
+awk '{print $1}' scores-table.txt
 # Your output should contain 6 lines, each with just one word on it.
