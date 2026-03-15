d52162bca6920a26ccd86bc18e2c5d3cdd1a2147
ZabihollahNamazi <zabihnamazi@gmail.com>
2026-03-15 22:00:21 +0000

    jq exercises are done

--- jq/script-05.sh
+++ jq/script-05.sh
@@ -5,3 +5,5 @@ set -euo pipefail
 # The input for this script is the scores.json file.
 # TODO: Write a command to output the names of each player, as well as their city.
 # Your output should contain 6 lines, each with two words on it.
+jq -r '.[] | [.name, .city] | join(", ")' scores.json  
+# .[] goes through each object   [.name, .city] = creates a temporary array for name and city  and then we use join to join name and city
