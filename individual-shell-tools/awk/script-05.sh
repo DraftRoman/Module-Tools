413b7319f0a2484ff4f11151ad97311772c26420
DraftRoman <roman.boxes@gmail.com>
2026-03-24 19:54:06 +0100

    awk The script-05.sh was missing in previos commitment

--- individual-shell-tools/awk/script-05.sh
+++ individual-shell-tools/awk/script-05.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 set -euo pipefail
+$ awk '{print $1, NF-2}' scores-table.txt
+Ahmed 3
+Basia 3
+Mehmet 3
+Leila 1
+Piotr 5
+Chandra 2
+
+
+
+subtract 2 (name + city)
 
 # TODO: Write a command to output just the names of each player along with the number of times they've played the game.
 # Your output should contain 6 lines, each with one word and one number on it.
