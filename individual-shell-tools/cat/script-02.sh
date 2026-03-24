e6a95e942f146db5c4cb314589f438ba8e23a0a5
DraftRoman <roman.boxes@gmail.com>
2026-03-24 21:07:56 +0100

    The cat command

--- individual-shell-tools/cat/script-02.sh
+++ individual-shell-tools/cat/script-02.sh
@@ -2,6 +2,13 @@
 
 set -euo pipefail
 
+$ cat ../helper-files/*
+Once upon a time...
+There was a house made of gingerbread.
+It looked delicious.
+I was tempted to take a bite of it.
+But this seemed like a bad idea...
+
 # TODO: Write a command to output the contents of all of the files inside the helper-files directory to the terminal.
 # Make sure you are only calling `cat` once.
 #
