2b138f8f253d1583c901593290cd75eb6cc01107
DraftRoman <roman.boxes@gmail.com>
2026-04-12 10:37:27 +0200

    now cat.js can  work with multiple files

--- implement-shell-tools/cat/cat.js
+++ implement-shell-tools/cat/cat.js
@@ -4,29 +4,31 @@ import { program } from "commander";
 program
   .name("myCat")
   .description("The re-implementation of the cat command")
-  .argument("<file>", "file to read")
+  .argument("<files...>", "files to read")
   .option("-n, --number", "number all lines")
   .option("-b, --number-nonblank", "number non-empty lines");
 
 program.parse();
 
 const options = program.opts();
-const path = program.args[0];
+const paths = program.args;
+let lineNumber = 1;
 
 try {
-    let content = await fs.readFile(path, "utf-8");
-    const lines = content.split("\n");
-    if (options.numberNonblank) {
-    let lineNumber = 1;
-        content = lines.map(line => {
-            if (line.trim() === "") return line;
-            return `${lineNumber++} ${line}`;
-        }).join("\n");
-    } else if (options.number) {
-        content = lines.map((line, index) => `${index + 1} ${line}`).join("\n");
-    }
+    for (const path of paths) {
+        let content = await fs.readFile(path, "utf-8");
+        const lines = content.split("\n");
+        if (options.numberNonblank) {
+            content = lines.map(line => {
+                if (line.trim() === "") return line;
+                return `${lineNumber++} ${line}`;
+            }).join("\n");
+        } else if (options.number) {
+            content = lines.map((line, index) => `${index + 1} ${line}`).join("\n");
+        }
     
-    console.log(content);
+        console.log(content);
+    }
 } catch (err) {
     console.error(`Error reading file: ${err.message}`);
     process.exit(1);
